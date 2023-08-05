0f0d54f3fdf9602affc5039f621b78e2e39eb868
Aditya <aditya298k@gmail.com>
2023-08-05 20:08:12 +0530

    Added Map using mapbox to tour page and started On LOGIN

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,7 +1,6 @@
 const Tour = require('../models/tourModel')
 const catchAsync = require('../utils/catchAsync')
 
-
 exports.getOverview = catchAsync(async (req,res,next)=>{
     //1. Get the Tour Data from the collection
     const tours = await Tour.find()
@@ -20,6 +19,11 @@ exports.getTour = catchAsync(async (req,res,next)=>{
     //we have populated guides in the tourmodel itself so we need not to polulate it again
     const tour = await Tour.find({_id:req.params.id}).populate('reviews')
     res.status(200).render('tour',{
+       title:`${tour[0].name} Tour`,
        tour:tour[0]   //since tour is an array
     })
+})
+
+exports.loginUser = catchAsync(async (req,res,next)=>{
+   res.status(200).render('login')
 })
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,7 +1,26 @@
 const express = require('express')
 const viewController = require('../controllers/viewController')
 
+//Very Important to make mapbox work alongside helmet
+const CSP = 'Content-Security-Policy';
+const POLICY =
+  "default-src 'self' https://*.mapbox.com ;" +
+  "base-uri 'self';block-all-mixed-content;" +
+  "font-src 'self' https: data:;" +
+  "frame-ancestors 'self';" +
+  "img-src http://localhost:8000 'self' blob: data:;" +
+  "object-src 'none';" +
+  "script-src https: cdn.jsdelivr.net cdnjs.cloudflare.com api.mapbox.com 'self' blob: ;" +
+  "script-src-attr 'none';" +
+  "style-src 'self' https: 'unsafe-inline';" +
+  'upgrade-insecure-requests;';
+
 const viewRouter = express.Router()
+
+viewRouter.use((req, res, next) => {
+    res.setHeader(CSP, POLICY);
+    next();
+  });
 
 //This is for the overview page 
 viewRouter.get('/',viewController.getOverview)
@@ -9,4 +28,6 @@ viewRouter.get('/',viewController.getOverview)
 //This is when enquiring for a specific tour
 viewRouter.get('/tour/:id',viewController.getTour)
 
+//This is the login Point
+viewRouter.get('/login',viewController.loginUser)
 module.exports = viewRouter
